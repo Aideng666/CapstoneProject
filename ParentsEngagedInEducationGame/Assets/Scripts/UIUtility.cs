5889ab726b2002339a5adaded0423e21838881ef
Tyler <tyler.wong@ontariotechu.net>
2023-03-01 14:32:07 -0500

    Added logo fade panel

--- ParentsEngagedInEducationGame/Assets/Scripts/UIUtility.cs
+++ ParentsEngagedInEducationGame/Assets/Scripts/UIUtility.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIUtility : MonoBehaviour
 {
     [Header("Canvas Groups")]
     [SerializeField] CanvasGroup startCanvasGroup;
+    [SerializeField] CanvasGroup logoImg;
     [SerializeField] CanvasGroup titleCanvasGroup;
     [SerializeField] CanvasGroup titleCanvasMainGroup;
 
     Tween fadeTween;
+    public float startFadeTime = 1f;
 
     void Start()
     {
-        StartCoroutine(StartFade());
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(1))
+        {
+            StartCoroutine(StartFade());
+        }
+
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(0))
+        {
+            StartCoroutine(LogoFade());
+        }
     }
 
     void Update()
@@ -53,9 +64,18 @@ public class UIUtility : MonoBehaviour
     }
 
     private IEnumerator StartFade()
+    {     
+        yield return new WaitForSeconds(startFadeTime);
+        FadeOut(startCanvasGroup, 1f);
+    }
+
+    private IEnumerator LogoFade()
     {
         yield return new WaitForSeconds(1f);
-        FadeOut(startCanvasGroup, 1f);
+        FadeIn(logoImg, 1f);
+        yield return new WaitForSeconds(3f);
+        FadeOut(logoImg, 1f);
+        StartCoroutine(StartFade());
     }
 
     private IEnumerator PlayButtonFade()
